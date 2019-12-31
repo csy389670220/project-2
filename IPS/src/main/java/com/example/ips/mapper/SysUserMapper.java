5e6b290dcba331e5284b35cb43fb07a358071700
chengsiyu_fb <389670220@qq.com>
2019-12-31 15:43:02 +0800

    u

---
+++ IPS/src/main/java/com/example/ips/mapper/SysUserMapper.java
@@ -0,0 +1,34 @@
+package com.example.ips.mapper;
+
+import com.example.ips.model.SysUser;
+
+public interface SysUserMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(SysUser record);
+
+    int insertSelective(SysUser record);
+
+    SysUser selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(SysUser record);
+
+    int updateByPrimaryKey(SysUser record);
+
+    /**
+     * 根据登录名查询数据
+     *
+     * @param loginName
+     * @return
+     */
+    SysUser selectByLoginName(String loginName);
+
+    /**
+     * 根据登录名查询角色
+     *
+     * @param loginName
+     * @return
+     */
+    String  selectRolesByLoginName(String loginName);
+
+}
